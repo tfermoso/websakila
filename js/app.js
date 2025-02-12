991d4f510d571f124f04513371813022e7c23522
tfermoso <tomas.fermoso@gmail.com>
2025-02-12 20:15:36 +0100

    peticion ajax post

--- js/app.js
+++ js/app.js
@@ -20,5 +20,21 @@ document.getElementById("peliculas").addEventListener("click", function () {
 document.getElementById("new_actor").addEventListener("click", function () {
     document.getElementById("contenido").hidden = true;
     document.getElementById("form_new_actor").hidden = false;
-})
+});
 
+document.getElementById("save_actor").addEventListener("click", function () {
+    let first_name = document.getElementById("name").value;
+    let last_name = document.getElementById("last_name").value;
+    let formData = new FormData();
+    formData.append("name", first_name);
+    formData.append("lastname", last_name);
+
+    fetch("http://192.168.100.166/apisakila/actores/new", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+});
