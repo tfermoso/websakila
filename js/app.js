385cbf31718e0541ac7bf2e5164d742e6a917dd8
tfermoso <tomas.fermoso@gmail.com>
2025-02-12 19:33:02 +0100

    asociando eventos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-document.getElementById("peliculas").addEventListener("click", function(){
+document.getElementById("peliculas").addEventListener("click", function () {
     fetch("http://192.168.100.166/apisakila/peliculas/all")
-    .then(response => response.json())
-    .then(data => {
-        let output = "<h2>Peliculas</h2>";
-        data.forEach(function(pelicula){
-            output += `
+        .then(response => response.json())
+        .then(data => {
+            let output = "<h2>Peliculas</h2>";
+            data.forEach(function (pelicula) {
+                output += `
                 <div>
                     <h3>${pelicula.title}</h3>
                     <p>${pelicula.description}</p>
                 </div>
             `;
+            });
+            document.getElementById("contenido").innerHTML = output;
         });
-        document.getElementById("contenido").innerHTML = output;
-    });
 });
+
+document.getElementById("new_actor").addEventListener("click", function () {
+    
+})
